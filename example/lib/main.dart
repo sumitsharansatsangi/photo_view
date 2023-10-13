98c38d4cdbb0f57ec603d0f256e3a01cad8574c2
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-10-13 16:25:37 +0530

    code updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,18 +4,28 @@ import './screens/home_screen.dart';
 void main() => runApp(MyApp());
 
 ThemeData theme = ThemeData(
-  primaryColor: Colors.black,
-  backgroundColor: Colors.white10,
+  useMaterial3: true,
+  // primaryColor: Colors.black,
+  colorSchemeSeed: Colors.white10,
   fontFamily: 'PTSans',
 );
 
+// ThemeData darkTheme = ThemeData(
+//   useMaterial3: true,
+//   // primaryColor: Colors.black,
+//   colorSchemeSeed: Colors.black,
+//   fontFamily: 'PTSans',
+// );
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Photo View Example App',
       theme: theme,
+      darkTheme: theme,
+      themeMode: ThemeMode.dark,
       home: Scaffold(
         body: HomeScreen(),
       ),
